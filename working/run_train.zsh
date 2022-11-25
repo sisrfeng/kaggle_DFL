7bcb1b3552bfcdb04c3295e0fdb913a7c87923b4
llwwff <5649701+llwwff@user.noreply.gitee.com>
2022-11-25 15:06:51 +0800

    25日15时

--- working/run_train.zsh
+++ working/run_train.zsh
@@ -1,4 +1,5 @@
-exp_PATH=new_input_size___Nov_24
+exp_PATH=new_input_size%%mixup%%$(date +"%-d号%H点")
+echo ${exp_PATH}
 # t可以改成rm , 我这里的t是扔进垃圾桶的意思, 避免旧实验干扰
 t exp/${exp_PATH}
 
@@ -6,17 +7,18 @@ t exp/${exp_PATH}
 #
 # p是改成python就行 (我把p设成了python和一些智能化的操作alias)
 p train_timm.py   ../work/imgs4train         \
-    --amp                                    \
-    --pretrained                             \
-    --num_classes   4                        \
-    --model         tf_efficientnet_b5_ap    \
-    --epochs        100                      \
-    --log_interval  5000                     \
-    --batch_size    2                        \
-    --which_metric  event_AP                 \
-    --bce_loss                               \
-    --input_size 3 540 960                   \
-    --output        ./exp                    \
+    --amp                                  \
+    --pretrained                           \
+    --num_classes   4                      \
+    --model         tf_efficientnet_b5_ap  \
+    --epochs        100                    \
+    --log_interval  5000                   \
+    --batch_size    2                      \
+    --which_metric  event_AP               \
+    --bce_loss                             \
+    --input_size 3 540 960                 \
+    --mixup         0.5                    \
+    --output        ./exp                  \
     --experiment    $exp_PATH
 
 
